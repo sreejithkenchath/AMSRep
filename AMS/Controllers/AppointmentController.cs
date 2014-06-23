60b6f95fc30b8aa707faa001f92653e7f21b1160
dennis7 <dennis.ms@live.com>
2014-06-23 11:02:20 +0800

    EF Generic Repository

---
+++ AMS/Controllers/AppointmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMS.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        //
+        // GET: /Appointment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Preferences()
+        {
+            return View();
+        }
+    }
+}
