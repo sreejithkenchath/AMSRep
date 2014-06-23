c4aec9b8882d0775ef758fab389a35a06330a5cd
Hari Nuti <harihara6@gmail.com>
2014-06-23 09:15:06 +0800

    Added reports

--- AMS/Controllers/UserController.cs
+++ AMS/Controllers/UserController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using AMS.AMS_BLL;
 using WebMatrix.WebData;
+using CrystalDecisions.CrystalReports.Engine;
+using System.IO;
 
 namespace AMS.Controllers
 {
@@ -20,6 +22,49 @@ namespace AMS.Controllers
             //ViewBag["Message"] = message;
             return View(users);
         }
+
+         public ActionResult ExportUsersList()
+         {
+             AMSEntities ae = new AMSEntities();
+             User uu = ae.Users.Where(e => e.MembershipUserID == WebSecurity.CurrentUserId).Single();
+             List<User> users = ae.Users.Where(e => e.CompanyID == uu.CompanyID).ToList();
+
+             var d =(from s in users
+                     select new
+                     {
+                         UserTitle = s.UserTitle ?? null,
+                         UserFirstName = s.UserFirstName ?? null,
+                         UserLastName = s.UserLastName ?? null,
+                         UserEmail = s.UserEmail ?? null,
+                         UserPhone = s.UserPhone ?? null, 
+                                                      }).ToList();
+
+
+             ReportDocument rd = new ReportDocument();
+             rd.Load(Path.Combine(Server.MapPath("~/Reports"), "MyUsers.rpt"));
+             rd.SetDataSource(d);
+
+
+             Response.Buffer = false;
+             Response.ClearContent();
+             Response.ClearHeaders();
+
+
+
+             try
+             {
+                 Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                 stream.Seek(0, SeekOrigin.Begin);
+                 return File(stream, "application/pdf", "MyusersList.pdf");
+             }
+
+             catch (Exception e)
+             {
+                 throw;
+             }
+
+             
+         }
 
         public ActionResult AddUser()
         {
